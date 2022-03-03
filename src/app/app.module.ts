b7787128c1986997418d1d83b36797ff60d3cb7e
Lucas Valentim <lucasv.27@hotmail.com>
2022-03-02 22:02:34 -0300

    Primeira Tela Consulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './views/menu/menu.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { ConsultaComponent } from './views/consulta/consulta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastroComponent,
+    ConsultaComponent,
   ],
   imports: [
     BrowserModule,
