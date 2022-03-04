6dd800823e513093a876bf06dcf0ca6b0591ebc1
Lucas Valentim <lucasv.27@hotmail.com>
2022-03-04 00:16:56 -0300

    Telas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { ConsultaComponent } from './views/consulta/consulta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ConsultaComponent
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  },
+  {
+    path: 'consulta',
+    component: ConsultaComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
